2ee9055b2ee3b46184c848f4d3051bd6e576069f
George Baptista <george@omame.com>
2017-08-28 00:47:26 +0900

    edit image.ex

--- lib/app/assets/image.ex
+++ lib/app/assets/image.ex
@@ -3,9 +3,14 @@ defmodule App.Assets.Image do
   import Ecto.Changeset
   alias App.Assets.Image
 
+  # GB arc
+  use Arc.Ecto.Schema
+
 
   schema "images" do
-    field :filename, :string
+
+    # GB arc
+    field :filename, App.ImageFile.Type
     field :name, :string
 
     timestamps()
@@ -15,6 +20,8 @@ defmodule App.Assets.Image do
   def changeset(%Image{} = image, attrs) do
     image
     |> cast(attrs, [:name, :filename])
+    # GB arc
+    |> cast_attachments(attrs, [:filename])
     |> validate_required([:name, :filename])
   end
 end
